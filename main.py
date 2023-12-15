8c1edc90e85fc22fc3bc2f1558a6256472b900fb
Nicolas BITAILLOU <nicolas.bitaillou@etu.univ-nantes.fr>
2023-12-15 11:41:02 +0100

    Test load dynamic img

--- main.py
+++ main.py
@@ -213,6 +213,15 @@ def content_new_style():
     loadVerticalTableAndChart ="assets/images/VerticalTableAndChart.png"
     st.write("Vertical Table and Chart (Histogramme)")
     st.image(loadVerticalTableAndChart)
+    st.markdown("---")
+    st.write("Image Dynamique URL")
+    st.write("lien google drive partagé")
+    #possible solution https://docs.streamlit.io/knowledge-base/tutorials/databases/public-gsheet
+    st.markdown("![Alt Text](https://drive.google.com/file/d/1UUl6KM01_qK2JD5ZJjCqURiZ3PTxW69Z/view?usp=sharing)")
+    st.write("lien google image")
+    st.markdown("![Alt Text](https://media.geeksforgeeks.org/wp-content/uploads/20200707212630/treemapplotlypython3.png)")
+    st.write("lien giphy.gif")
+    st.markdown("![test](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")
 
 # Comparison page Content
 def content_comparison():
