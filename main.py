9b23f18bfab5c79a542c287c1e4ae70e3acbd754
Stevehunn <nicolas.bitaillou@etu.univ-tours.fr>
2023-12-22 15:42:54 +0100

    Add file for each page

--- main.py
+++ main.py
@@ -95,43 +95,44 @@ def getCheminForImage(nomfichier):
     result = f"assets/plots/{parts[1]}_plot.svg"
     return result
 
-# 
+# Version Windows
 target_classes = []
-# for file in glob.glob("assets/plots/*.svg"):
+for file in glob.glob("assets/plots/*.svg"):
 #     # Utilisez split pour séparer le chemin du fichier
-#     parts = file.split("/")
+    parts = file.split("/")
     
 #     # Extrait le premier élément après "assets"
-#     fname = parts[1]  # parts[0] est "assets", parts[1] est "plots"
+    fname = parts[1]  # parts[0] est "assets", parts[1] est "plots"
 
-#     parts2 = fname.split("plots")
+    parts2 = fname.split("plots")
 #     # Extrait le premier élément après "plots"
-#     newFname =parts2[1]
+    newFname =parts2[1]
 
 #     # Utiliser split pour séparer la chaîne en fonction de "\"
-#     parts = newFname.split("\\")
+    parts = newFname.split("\\")
 
 #     # Concaténer les parties avec "schema:"
-#     newFname ="".join(parts[1:])
+    newFname ="".join(parts[1:])
 
 #     # Supprime l'extension ".svg"
-#     cname = newFname.split("_plot.svg")[0]
+    cname = newFname.split("_plot.svg")[0]
     
 #     # Ajoute à la liste avec le préfixe "schema:"
-#     target_classes.append(f"schema:{cname}")
-
-for file in glob.glob("assets/plots/*.svg"):
-    # Utilisez split pour séparer le chemin du fichier
-    parts = file.split("/")
-
-    fname = parts[2] 
-    parts2 = fname.split(".")
-    # Extrait le premier élément après "plots"
-    newFname =parts2[0]
-
-    cname = newFname.split("_plot")[0]
-    # if cname != "Intangible":
     target_classes.append(f"schema:{cname}")
+
+# Version Mac
+#for file in glob.glob("assets/plots/*.svg"):
+    # Utilisez split pour séparer le chemin du fichier
+ #   parts = file.split("/")
+
+  #  fname = parts[2]
+   # parts2 = fname.split(".")
+    # Extrait le premier élément après "plots"
+    #newFname =parts2[0]
+
+    #cname = newFname.split("_plot")[0]
+    # if cname != "Intangible":
+    #target_classes.append(f"schema:{cname}")
     
     
 
